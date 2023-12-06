7f7d6b4fc5b1a81c1cb26cf4209e6805ab5b70d0
spacemapache <142768034+spacemapache@users.noreply.github.com>
2023-12-06 17:07:59 +0800

    js for button change

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// menu change
+let menu = document.querySelector("#drink-menu");
+menu.innerHTML = `HANKY PANKY
+<br />
+Junmai namachozo sake, strawberry, yuzu, sparkling rose, bitters
+<br/>
+$17
+<br />
+BLIND DATE
+
+Shochu, shushing, lime rose water, soda water
+
+$17
+
+BINGO BANGO
+
+Nigori sake, butterfly pea, yuzu, lemon, jasmine water
+
+$17
+
+WASABI MARGARITA
+
+Agave wine, cucumber, junta sake, lime, wasabi, togarashi
+
+$16
+
+SHEEBA SHEEBA
+
+Agave wine, blanc vermouth, ponzu, lime, prickly pear
+
+$16
+
+ROASTED BARLEY OLD FASHIONED
+
+Barrel rested shook, roasted barley, hickory- smoked cherry bitters
+
+$16
+
+`;
