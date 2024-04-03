4db80dc2f12fe3a6ee8b5b3c9f115222bc8facc0
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-04-03 14:35:30 -0700

    added js for buttons to switch between menus

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
 // menu change
-function menuChange() {
-  let drinkMenu = `HANKY PANKY``Junmai namachozo sake, strawberry, yuzu, sparkling rose, bitters``$17``BLIND DATE
-Shochu, shushing, lime rose water, soda water
-$17
-BINGO BANGO
-Nigori sake, butterfly pea, yuzu, lemon, jasmine water
-$17
-WASABI MARGARITA
-Agave wine, cucumber, junta sake, lime, wasabi, togarashi
-$16
-SHEEBA SHEEBA
-Agave wine, blanc vermouth, ponzu, lime, prickly pear
-$16
-ROASTED BARLEY OLD FASHIONED
-Barrel rested shook, roasted barley, hickory- smoked cherry bitters
-$16`;
-}
-
-let drinkElement = document.querySelector("#menu");
-drinkElement.innerHTML = drinkMenu;
-drinkMenu.addEventListener("click", drinkElement);
 
 //menu change from food to drink - innerhtml
 // when submit button is hit, alert we have received your request
+// if form is empty = else say please fill in the form
+
+// submit btn
+function submitBtn() {
+  event.preventDefault();
+  alert("We have received your reservation request");
+}
+
+// function for switching menus
+function drinkBtn(event) {
+  event.preventDefault();
+  let drinkMenu = document.getElementById("menuItems");
+  drinkMenu.innerHTML = `<li>HANKY PANKY</li>
+              <li>
+                Junmai namachozo sake, strawberry, yuzu, sparkling rose, bitters
+              </li>
+              <li>BLIND DATE</li>
+              <li>Shochu, shushing, lime rose water, soda water</li>
+              <li>BINGO BANGO</li>
+              <li>Nigori sake, butterfly pea, yuzu, lemon, jasmine water</li>
+              <li>WASABI MARGARITA</li>
+              <li>Agave wine, cucumber, junta sake, lime, wasabi, togarashi</li>`;
+}
+
+function foodBtn(event) {
+  event.preventDefault();
+  let foodMenu = document.getElementById("menuItems");
+  foodMenu.innerHTML = `<li>BIG FIISH</li>
+              <li>
+                Edamame, seaweed salad cucumber salad, aged tuna sashimi, 2
+                salmon with lemon zest, 2 albacore garlic ponzu pickled onion, 2
+                yellowtail jalapeño negi, 2 stripped bass ponzu togarashi negi,
+                1 tuna jalapeño garlic aioli sesame, 1 snapper yuzu koshu wasabi
+                negi, 4 pieces salmon cut roll, 4 pieces spicy tuna roll
+              </li>
+              <li>LITTLE FIISH</li>
+              <li>
+                Edamame, seaweed salad, 2 salmon with lemon zest, 1 albacore
+                garlic ponzu pickled onion, 2 yellowtail jalapeño Negi, 1
+                stripped bass ponzu togarashi Negi, 1 tuna smoked soy wasabi
+                sashimi, 4 pieces salmon cut roll, 4 pieces spicy Tuna roll
+              </li>`;
+}
